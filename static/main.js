a3ebb506b7d65ca3bdc761bd43fa47ff40a7cbbf
Redderdev <inf4198@hs-worms.de>
2026-07-21 15:35:56 +0200

    ui_changes

--- static/main.js
+++ static/main.js
@@ -1,1 +1,7 @@
 // Gemeinsame Hilfsfunktionen – werden in allen Templates genutzt
+
+// Haupt-Navigation ein-/ausklappen (mehr Platz für Inhalt, z.B. CV-Editor)
+function toggleNav() {
+  const collapsed = document.body.classList.toggle('nav-collapsed');
+  try { localStorage.setItem('nav_collapsed', collapsed ? '1' : ''); } catch (e) {}
+}
